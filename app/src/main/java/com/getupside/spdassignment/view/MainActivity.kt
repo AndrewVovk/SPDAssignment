7e7ad65882dbfbb5388b576c596e3d49b20b9aa7
Andriy Vovk <andrijvovk1988@gmail.com>
2019-06-04 04:01:07 +0300

    final

--- app/src/main/java/com/getupside/spdassignment/view/MainActivity.kt
+++ app/src/main/java/com/getupside/spdassignment/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.getupside.spdassignment.view
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
@@ -17,52 +18,77 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val LAYOUT_MANAGER_STATE = "layoutManagerState"
+    }
+
+    private val viewModel by lazy { ViewModelProviders.of(this)[MainViewModel::class.java] }
+    private val mainProgressBar by lazy { findViewById<View>(R.id.progress_bar_main) }
+    private val lazyProgressBar by lazy { findViewById<View>(R.id.progress_bar_lazy) }
+    private val recyclerView by lazy { findViewById<RecyclerView>(R.id.recycler_view) }
+    private val rootView by lazy { findViewById<View>(R.id.root) }
+    private val adapter by lazy {
+        SimpleAdapter(
+            { viewModel.data.size },
+            { ImageViewHolder(layoutInflater.inflate(R.layout.item_image, it, false)) },
+            { holder, position -> holder.bind(this, viewModel.data[position]) },
+            ImageViewHolder::destroy
+        )
+    }
+
+    private val imageDataStateObserver = Observer<ImageDataState> { state ->
+        when (state) {
+            ImageDataState.LOADING -> {
+                mainProgressBar.visibility = VISIBLE
+            }
+            ImageDataState.DATA_CREATED -> {
+                mainProgressBar.visibility = GONE
+                lazyProgressBar.visibility = GONE
+                initRecyclerView()
+            }
+            ImageDataState.LOADING_MORE -> {
+                lazyProgressBar.visibility = VISIBLE
+            }
+            ImageDataState.DATA_ADDED -> {
+                mainProgressBar.visibility = GONE
+                lazyProgressBar.visibility = GONE
+                recyclerView.adapter?.notifyDataSetChanged() ?: initRecyclerView()
+            }
+            else -> mainProgressBar.visibility = GONE
+        }
+    }
+
+    private fun initRecyclerView() {
+        recyclerView.adapter = adapter
+        layoutManagerState?.let { recyclerView.layoutManager?.onRestoreInstanceState(it) }
+    }
+
+    private val connectivityObserver = Observer<Boolean> {
+        if (!it) {
+            lazyProgressBar.visibility = GONE
+            Snackbar
+                .make(rootView, R.string.no_internet_connection, Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.retry) {
+                    viewModel.retryToConnect()
+                }.show()
+        }
+    }
+
+    private var layoutManagerState: Parcelable? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewModel = ViewModelProviders.of(this)[MainViewModel::class.java]
-        val mainProgressBar = findViewById<View>(R.id.progress_bar_main)
-        val lazyProgressBar = findViewById<View>(R.id.progress_bar_lazy)
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        val rootView = findViewById<View>(R.id.root)
         recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        layoutManagerState = savedInstanceState?.getParcelable(LAYOUT_MANAGER_STATE)
 
-        viewModel.data.state.observe(this, Observer { state ->
-            when (state) {
-                ImageDataState.LOADING -> {
-                    mainProgressBar.visibility = VISIBLE
-                }
-                ImageDataState.DATA_CREATED -> {
-                    mainProgressBar.visibility = GONE
-                    lazyProgressBar.visibility = GONE
-                    recyclerView.adapter = SimpleAdapter(
-                        { viewModel.data.size },
-                        { ImageViewHolder(layoutInflater.inflate(R.layout.item_image, it, false)) },
-                        { holder, position -> holder.bind(this, viewModel.data[position]) },
-                        ImageViewHolder::destroy
-                    )
-                }
-                ImageDataState.LOADING_MORE -> {
-                    lazyProgressBar.visibility = VISIBLE
-                }
-                ImageDataState.DATA_ADDED -> {
-                    lazyProgressBar.visibility = GONE
-                    recyclerView.adapter?.notifyDataSetChanged()
-                }
-                else -> mainProgressBar.visibility = GONE
-            }
-        })
+        viewModel.data.state.observe(this, imageDataStateObserver)
+        viewModel.connectivityLiveData.observe(this, connectivityObserver)
+    }
 
-        viewModel.connectivityLiveData.observe(this, Observer {
-            if (!it) {
-                lazyProgressBar.visibility = GONE
-                Snackbar
-                    .make(rootView, R.string.no_internet_connection, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(R.string.retry) {
-                        viewModel.retryToConnect()
-                    }.show()
-            }
-        })
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(LAYOUT_MANAGER_STATE, recyclerView.layoutManager?.onSaveInstanceState())
     }
 }
